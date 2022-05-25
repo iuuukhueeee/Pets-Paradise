e60dd5ebf4638250b603ad47fd66700be040b3ad
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2022-05-25 21:27:01 +0700

    implemented Login/out

--- src/main/java/com/user/UserDAO.java
+++ src/main/java/com/user/UserDAO.java
@@ -1,25 +1,94 @@
 package com.user;
 
-import com.utils.DButils;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.utils.*;
 
 public class UserDAO {
 
-    public boolean login(String username, String password) {
+    private static final String LOGIN = "SELECT Username, Name, Password, Email, PhoneNumber, RoleID FROM Users WHERE Username=? AND Password=? AND Status=1";
+    private static final String CHECK_DUPLICATE = "SELECT Name FROM Users WHERE Username=?";
+
+    public UserDTO checkLogin(String Username, String Password) throws SQLException {
+        UserDTO user = null;
+
         Connection conn = null;
         PreparedStatement ptm = null;
         ResultSet rs = null;
 
         try {
-
             conn = DButils.getConnection();
-            System.out.println(conn != null);
+            if (conn != null) {
+                ptm = conn.prepareStatement(LOGIN);
+                ptm.setString(1, Username);
+                ptm.setString(2, Password);
+                rs = ptm.executeQuery();
+                if (rs.next()) {
+                    String username = rs.getString("Username");
+                    String Name = rs.getString("Name");
+                    String Email = rs.getString("Email");
+                    String PhoneNumber = rs.getString("PhoneNumber");
+                    String RoleID = rs.getString("RoleID");
+                    user = new UserDTO(username, Name, "",Email, PhoneNumber, RoleID );
+                }
+            }
         } catch (Exception e) {
-
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
-        return true;
+        return user;
     }
+
+    public boolean checkDuplicate(String userID) throws SQLException {
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DButils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(CHECK_DUPLICATE);
+                ptm.setString(1, userID);
+                rs = ptm.executeQuery();
+                if (rs.next()) {
+                    check = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+        }
+        return check;
+    }
+
+
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,47 @@
+<%--
+    Document   : login
+    Created on : Mar 11, 2022, 9:02:11 PM
+    Author     : hd
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Login Page</title>
+</head>
+<body>
+<form action="MainController" method="POST">
+    <fieldset>
+        <legend><h2>Login</h2></legend>
+        <table>
+            <tr>
+                <td>Username</td>
+                <td>
+                    <input type="text" required name="Username" placeholder="Username"/>
+                </td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>
+                    <input type="password" required name="Password" placeholder="Password"/>
+                </td>
+            </tr>
+        </table>
+        <p>
+            <input type="submit" name="action" value="Login"/>
+            <input type="reset" value="Reset"/>
+        </p>
+        <%
+            String error = (String) request.getAttribute("ERROR");
+            if (error == null) {
+                error = "";
+            }
+        %>
+        <p><%=error%></p>
+
+    </fieldset>
+</form>
+</body>
+</html>
