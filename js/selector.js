b64a8a23b8ee9d7dcc4c221a1debbf8e1f6c1ba2
Dieu@nh1411 <vuongtrandieuanh14@gmail.com>
2022-06-30 11:02:30 +0700

    fixed-input_file

--- js/selector.js
+++ js/selector.js
@@ -10,15 +10,3 @@ let getSelector = () => {
       "none");
   }
 };
-function readURL(input) {
-  if (input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function (e) {
-          $('#blah')
-              .attr('src', e.target.result);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-  }
-}
