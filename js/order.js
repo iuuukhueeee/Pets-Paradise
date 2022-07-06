28a12a5f756472d6d59c79c24fee78a3c6bc9a6d
khang20112002 <khangdcse161156@fpt.edu.vn>
2022-07-06 07:16:00 +0700

    Order_Table_Expand_Done

--- js/order.js
+++ js/order.js
@@ -1,11 +1,5 @@
-$('.arrow').on('click', function() {
-    $(this).toggleClass('active');
-});
-
-$('.arrow--l-r').on('click', function() {
-    $(this).toggleClass('left right');
-});
 
 function showHideRow(row) {
     $("#" + row).toggle();
 }
+
