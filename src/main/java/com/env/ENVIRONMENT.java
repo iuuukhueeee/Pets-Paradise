7f19f0222711e7c3b82b867ef43f6b974fa3b873
hl11jp <nguyenducthien9@gmail.com>
2022-07-06 14:38:36 +0700

    env

---
+++ src/main/java/com/env/ENVIRONMENT.java
@@ -0,0 +1,6 @@
+package com.env;
+
+public class ENVIRONMENT {
+    static public String usernameEmail = "thisisthientesting@gmail.com";
+    static public String passwordEmail = "mdkyzhywmatgpelk";
+}
