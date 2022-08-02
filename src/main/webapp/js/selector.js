186dbdbb2bd7f837859b4e86460b6c7c7ba0e460
hl11jp <nguyenducthien9@gmail.com>
2022-08-02 11:38:28 +0700

    selector

--- src/main/webapp/js/selector.js
+++ src/main/webapp/js/selector.js
@@ -11,17 +11,17 @@ let getSelector = () => {
   }
 };
 
-let actualTotal = 0;
-
 [...document.querySelectorAll(".item")].forEach((item) => {
+  console.log(item)
   const price = item.querySelector("[price]");
   const total = item.querySelector("[total]");
-  const checkoutTotal = item.querySelector("[checkout--total]");
-  item.querySelector("input").addEventListener("change", (e) => {
+  const checkoutTotal = document.querySelector(
+      "#form > div > div > div > div > div.finish > div > span.bg-indigo-100.text-indigo-800.text-sm.font-medium.mr-2.px-2\\.5.py-0\\.5.rounded.dark\\:bg-indigo-200.dark\\:text-indigo-900"
+  );
+  item.querySelector("input#qty.qty").addEventListener("change", (e) => {
     const quantity = e.target.value;
     total.textContent = parseInt(price.textContent) * parseInt(quantity);
-    actualTotal = total.textContent;
-    checkoutTotal.textContent = actualTotal;
-    console.log(actualTotal);
+    actualTotal -= parseFloat(total.textContent);
+    checkoutTotal.innerHTML = actualTotal;
   });
 });
